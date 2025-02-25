9757840f7db64ad9578d919e4bf6c38fc59e3688
Paul Denlinger <pmdenlinger@icloud.com>
2025-02-25 14:45:38 -0800

    Add object and multiline object to JS file

--- js-intro/app.js
+++ js-intro/app.js
@@ -1,6 +1,15 @@
 let age = 32;
 let userName = "Max";
 let hobbies = ["Sports", "Cooking", "Reading"];
+let job = { title: "Developer", place: "New York", salary: 50000 };
+
+let job1 = { 
+    title: "Developer", 
+    place: "New York", 
+    salary: 50000 
+};
+
+
 
 alert(hobbies);
-alert(hobbies[2]);
+
