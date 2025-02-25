f523813e567f0994105153a02b51f1a4079f5f4d
Paul Denlinger <pmdenlinger@icloud.com>
2025-02-25 13:39:01 -0800

    Add array to JS alert

--- js-intro/app.js
+++ js-intro/app.js
@@ -1,8 +1,6 @@
 let age = 32;
-let greetingText = "Hi, I'm Max!!!!";
-alert(greetingText);
-alert(greetingText);
-greetingText = "Hi, I'm really Max";
-alert(greetingText);
-alert(age);
-alert(age);
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+
+alert(hobbies);
+alert(hobbies[2]);
