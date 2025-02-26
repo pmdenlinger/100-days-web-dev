1bfbf8a2fc21b18e3c27f228ca9acd68b2bb1712
Paul Denlinger <pmdenlinger@icloud.com>
2025-02-26 11:04:03 -0800

    Create calculateAdultYears() function

--- js-intro/app.js
+++ js-intro/app.js
@@ -3,14 +3,17 @@ let userName = "Max";
 let hobbies = ["Sports", "Cooking", "Reading"];
 let job = { title: "Developer", place: "New York", salary: 50000 };
 
-let job1 = { 
-    title: "Developer", 
-    place: "New York", 
-    salary: 50000 
+let job1 = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
 };
 
+let adultYears;
 
+function calculateAdultYears() {
+  adultYears = age - 18;
+}
 
-alert(hobbies);
-alert(job1.salary);
-
+calculateAdultYears();
+alert(adultYears);
