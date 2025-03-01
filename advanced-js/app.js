42c2e3df16b7c5bcaecfd68290998f697bb7af39
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-01 11:32:39 -0800

    "Add query selector method to app.js and change link to Udemy"

--- advanced-js/app.js
+++ advanced-js/app.js
@@ -1,1 +1,4 @@
 document.body.children[1].children[0].href = "https://www.google.com";
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://www.udemy.com";
