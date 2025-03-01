b0a13e45ea15282505ec0cf47a4c8a7be1e09dd7
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-01 11:39:41 -0800

    Add querySelector method to app.js

--- advanced-js/app.js
+++ advanced-js/app.js
@@ -2,3 +2,6 @@ document.body.children[1].children[0].href = "https://www.google.com";
 
 let anchorElement = document.getElementById("external-link");
 anchorElement.href = "https://www.udemy.com";
+
+let anchorElement2 = document.querySelector("#external-link");
+anchorElement2.href = "https://www.github.com";
