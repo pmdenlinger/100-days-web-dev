cb0069ef522b232c4535bb649b561a511eb47967
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-01 15:35:18 -0800

    Answers to exercise

--- advanced-js/app.js
+++ advanced-js/app.js
@@ -8,16 +8,21 @@ anchorElement2.href = "https://www.github.com";
 
 // Exercise Time!
 
-// 1. Select the <h1> element by "drilling into the DOM" and 
+// 1. Select the <h1> element by "drilling into the DOM" and
 //    save it in a variable with a name of your choice
+let heading = document.body.children[0];
 // 2. Use the variable from (1) and get access to the "parent"
 //    element of the stored <h1> element (i.e. to the <body> element)
-//    BONUS: Try using the variable from (1) to get access to the 
+//    BONUS: Try using the variable from (1) to get access to the
 //    sibling element (i.e. the <p> element next to the <h1> element)
+let headingParent = document.body.children[0].parentElement;
 // 3. Select the <h1> element with getElementById and store in
 //    the same or a new variable (up to you)
+let headingById = document.getElementById("heading");
 // 4. Select the second <p> element with querySelector (you might
-//    need to add something in the HTML code, e.g. a class) 
+//    need to add something in the HTML code, e.g. a class)
 //    and store it in a new variable with a name of your choice
+let secondPara = document.querySelector(".secondParagraph");
 // 5. BONUS TASK: Try changing the text content of the <p> element
 //    you selected in (4) and set it to any other text of your choice
+secondPara.textContent = "This is changed text";
