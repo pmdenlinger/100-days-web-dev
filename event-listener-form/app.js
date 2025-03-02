dfff1963ea269eb97d2ed522768f14830d77206d
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-01 17:30:49 -0800

    Add input form, event listener and retrieveUserInput function

--- event-listener-form/app.js
+++ event-listener-form/app.js
@@ -6,3 +6,14 @@ function changeParagraphText() {
 }
 
 paragraphElement.addEventListener("click", changeParagraphText);
+
+let inputElement = document.querySelector("input");
+
+function retrieveUserInput(){
+    let enteredText = inputElement.value;
+    console.log(enteredText);
+}
+
+inputElement.addEventListener("input", retrieveUserInput);
+
+
