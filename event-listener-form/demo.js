dccd8b4ebdac47f49ec97bfdeac412fac79ba9ea
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-03 14:01:58 -0800

    Add else statement to remove warning if remaining characters > 10

--- event-listener-form/demo.js
+++ event-listener-form/demo.js
@@ -2,16 +2,19 @@ let productNameInputElement = document.getElementById("product-name");
 let remainingCharsElement = document.getElementById("remaining-chars");
 
 function updateRemainingCharacters(event) {
-    let enteredText = event.target.value;
-    let enteredTextLength = enteredText.length;
+  let enteredText = event.target.value;
+  let enteredTextLength = enteredText.length;
 
-    let remainingCharacters = 60 - enteredTextLength;
-    remainingCharsElement.textContent = remainingCharacters;
+  let remainingCharacters = 60 - enteredTextLength;
+  remainingCharsElement.textContent = remainingCharacters;
 
-    if (remainingCharacters <= 10) {
-        remainingCharsElement.classList.add("warning");
-        productNameInputElement.classList.add("warning");
-    }
+  if (remainingCharacters <= 10) {
+    remainingCharsElement.classList.add("warning");
+    productNameInputElement.classList.add("warning");
+  } else {
+    remainingCharsElement.classList.remove("warning");
+    productNameInputElement.classList.remove("warning");
+  }
 }
 
 productNameInputElement.addEventListener("input", updateRemainingCharacters);
