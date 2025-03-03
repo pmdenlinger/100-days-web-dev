7844bd88d6015c35950a0e077af3189f4173ebce
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-03 14:39:16 -0800

    Add for-of loop for arrays

--- event-listener-form/loops.js
+++ event-listener-form/loops.js
@@ -1,3 +1,10 @@
 for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
+
+// forof loop used for arrays
+const users = ["Max", "Anna", "Joel"];
+
+for (const user of users) {
+  console.log(user);
+}
