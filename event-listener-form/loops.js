3047e6a06779899f6917ecfc960584df22032649
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-04 14:58:10 -0800

    Add for in loop with propertyName only output

--- event-listener-form/loops.js
+++ event-listener-form/loops.js
@@ -8,3 +8,17 @@ const users = ["Max", "Anna", "Joel"];
 for (const user of users) {
   console.log(user);
 }
+
+// For in loop used on objects
+
+const loggedInUser = {
+  name: "Max", 
+  age: 32,
+  isAdmin: true
+};
+
+// Outputs propertyName only
+
+for (const propertyName in loggedInUser) {
+  console.log(propertyName);
+}
